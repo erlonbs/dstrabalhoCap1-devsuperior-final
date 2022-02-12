5b153fc300b671117077c49dd2a13b6733c850e3
Erlon Barbosa <erlonecia@msn.com>
2022-02-12 19:17:47 -0300

    Backend project trabalhoCap1 created

---
+++ backend/src/main/java/com/devsuperior/dstrabalhoCap1/DstrabalhoCap1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dstrabalhoCap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DstrabalhoCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DstrabalhoCap1Application.class, args);
+	}
+
+}
