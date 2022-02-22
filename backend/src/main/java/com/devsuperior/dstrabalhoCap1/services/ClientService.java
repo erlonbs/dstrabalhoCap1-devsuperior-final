674a1d3014b8875d47098a3670b4172eaaf57123
Erlon Barbosa <erlonecia@msn.com>
2022-02-21 23:36:59 -0300

    Update Client

--- backend/src/main/java/com/devsuperior/dstrabalhoCap1/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dstrabalhoCap1/services/ClientService.java
@@ -1,9 +1,10 @@
 package com.devsuperior.dstrabalhoCap1.services;
 
 import java.util.List;
-import java.util.Locale.Category;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dstrabalhoCap1.dto.ClientDTO;
 import com.devsuperior.dstrabalhoCap1.entities.Client;
 import com.devsuperior.dstrabalhoCap1.repositories.ClientRepository;
-import com.devsuperior.dstrabalhoCap1.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dstrabalhoCap1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -28,21 +29,43 @@ public class ClientService {
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
 
 	}
-@Transactional(readOnly = true)
+
+	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
-	 Optional<Client> obj=repository.findById(id);
-	 Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new ClientDTO(entity);
 	}
 
-@Transactional 
-public ClientDTO insert(ClientDTO dto) {
-	Client entity  = new Client();
-	entity.setName(dto.getName());
-	entity = repository.save(entity);
-	return new ClientDTO(entity);
-			
-}
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
 
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirtDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirtDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
 
 }
